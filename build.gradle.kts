62fd9f1426e3e7a92587960006d7bc116846facf
foameraserblue <foameraserblue@gmail.com>
2025-09-06 18:11:08 +0900

    :wrench: chore: kotest, 스웨거 디펜더시 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,11 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    testImplementation("io.kotest:kotest-assertions-core-jvm:6.0.1")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:6.0.1")
+
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.12")
 }
 
 kotlin {
