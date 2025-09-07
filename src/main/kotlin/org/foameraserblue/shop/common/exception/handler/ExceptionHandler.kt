668743d0c71eca6c4ac9475b547e531370b6ca58
foameraserblue <foameraserblue@gmail.com>
2025-09-08 03:13:10 +0900

    :wrench: chore : 예외핸들러 보강

--- src/main/kotlin/org/foameraserblue/shop/common/exception/handler/ExceptionHandler.kt
+++ src/main/kotlin/org/foameraserblue/shop/common/exception/handler/ExceptionHandler.kt
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.servlet.resource.NoResourceFoundException
 
 // 테스트 오류 확인용으로 작성
 @RestControllerAdvice
@@ -24,12 +25,18 @@ class ExceptionHandler {
         return response(exception)
     }
 
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NoResourceFoundException::class)
+    fun handleNoResourceFoundException(exception: NoResourceFoundException): ErrorMessageDto {
+        return response(exception)
+    }
+
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception::class)
     fun handleTotalException(exception: Exception): ErrorMessageDto {
         log.error("서버 애러", exception)
         return ErrorMessageDto()
     }
-    
+
     private fun response(exception: Exception) = ErrorMessageDto(exception.message.toString())
 }
